5821ade84e61e016e77d963a31145c553e743562
Janat Taerakul <janat.taerakul@gmail.com>
2020-06-07 03:37:33 +0900

    Initial commit

---
+++ templates/codeforces.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define PUB push_back
+#define POB pop_back
+#define MP(x,y) make_pair(x,y)
+#define F first
+#define S second
+using namespace std;
+typedef long long int lli;
+typedef pair<int,int> pii;
+int main(){
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+}
