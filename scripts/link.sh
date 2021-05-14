95c3ed5b395a6f103b91d1e9d9b42e6a495216f9
Janat Taerakul <janat.taerakul@gmail.com>
2021-05-14 14:14:31 +0900

    Added .gitconfig to repostory

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,4 +2,4 @@ ln -sfF ~/.janat/.zshrc ~/.zshrc
 ln -sfF ~/.janat/.tmux.conf ~/.tmux.conf
 ln -sfF ~/.janat/.p10k.zsh ~/.p10k.zsh
 ln -sfF ~/.janat/.vim ~/.vim
-
+ln -sfF ~/.janat/.gitconfig ~/.gitconfig
--- templates/codeforces.cpp
+++ templates/codeforces.cpp
@@ -3,6 +3,8 @@
 #define MP(x,y) make_pair(x,y)
 #define F first
 #define S second
+#define LOOP(i,m,n) for(int i=(int)(m) ; i < (int) (n) ; ++i )
+#define lp(i,n) REP(i,0,n)
 using namespace std;
 typedef long long int lli;
 typedef pair<int,int> pii;
