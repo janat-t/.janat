838dad2164c238a9e27b30f73ad8b2cf822a7713
Janat Taerakul <janat.taerakul@gmail.com>
2021-05-26 01:21:19 +0900

    adds loop shortcuts to codeforces template, adds material design itermcolors to repo

--- templates/codeforces.cpp
+++ templates/codeforces.cpp
@@ -3,8 +3,11 @@
 #define MP(x,y) make_pair(x,y)
 #define F first
 #define S second
-#define LOOP(i,m,n) for(int i=(int)(m) ; i < (int) (n) ; ++i )
-#define lp(i,n) REP(i,0,n)
+#define loop(i,m,n) for(int i=(int)(m) ; i < (int) (n) ; ++i )
+#define rloop(i,m,n) for(int i=(int)(m) ; i > (int) (n) ; --i )
+#define lp(i,n) loop(i,0,n)
+#define rlp(i,n) rloop(i,n,0)
+#define mod 1000000007
 using namespace std;
 typedef long long int lli;
 typedef pair<int,int> pii;
