6064b6b5f13aeeeff0a39f52ada84ee979b8651e
Janat Taerakul <janat.taerakul@gmail.com>
2023-08-24 18:23:47 +0900

    Add files from indeed branch to master branch

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,7 +1,42 @@
-ln -sfF ~/.janat/zshrc ~/.zshrc
-ln -sfF ~/.janat/bashrc ~/.bashrc
-ln -sfF ~/.janat/tmux.conf ~/.tmux.conf
-ln -sfF ~/.janat/p10k.zsh ~/.p10k.zsh
-ln -sfF ~/.janat/vim ~/.vim
-ln -sfF ~/.janat/gitconfig ~/.gitconfig
+#!/bin/sh
+
+mode='zsh'
+
+if [[ -n $1 ]]; then
+    mode=$1
+fi
+
+
+cp ~/.gitconfig ~/.gitconfig.old
+cp ~/.zshrc ~/.zshrc.old
+cp ~/.bashrc ~/.bashrc.old
+cp ~/.tmux.conf ~/.tmux.conf.old
+cp ~/.vimrc ~/.vimrc.old
+
+case $mode in
+    "zsh")
+        echo "Linking config files for ZSH"
+        ln -sfFv ~/.janat/zshrc.zsh ~/.zshrc
+        ln -sfFv ~/.janat/gitconfig.cfg ~/.gitconfig
+        ln -sfFv ~/.janat/p10k.zsh ~/.p10k.zsh;;
+    "bash")
+        echo "Linking config files for Bash"
+        ln -sfFv ~/.janat/bashrc.bash ~/.bashrc
+        ln -sfFv ~/.janat/gitconfig.cfg ~/.gitconfig;;
+    "indeed" | "indeed-zsh")
+        echo "Linking config files for ZSH Indeed"
+        ln -sfFv ~/.janat/zshrc.indeed.zsh ~/.zshrc
+        ln -sfFv ~/.janat/gitconfig.indeed.cfg ~/.gitconfig
+        ln -sfFv ~/.janat/p10k.zsh ~/.p10k.zsh;;
+    "indeed-bash")
+        echo "Linking config files for Bash Indeed"
+        ln -sfFv ~/.janat/bashrc.indeed.bash ~/.bashrc
+        ln -sfFv ~/.janat/gitconfig.cvm.cfg ~/.gitconfig;;
+esac
+
+echo "Linking base config files"
+ln -sfFv ~/.janat/tmux.conf ~/.tmux.conf
+[[ -d ~/.vim ]] && (rm ~/.vim.old; mv ~/.vim ~/.vim.old)
+ln -sfFv ~/.janat/vim ~/.vim
 vim -c 'PlugInstall' -c 'qa!'
+echo "Linking complete!"
--- zshrc.zsh
+++ zshrc.zsh
@@ -1,10 +1,6 @@
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
-# if [[ -r "/opt/intel/oneapi/setvars.sh" ]]; then
-#     source "/opt/intel/oneapi/setvars.sh"
-# fi
-
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
     source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
