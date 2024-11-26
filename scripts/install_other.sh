f653803c5bb9655c03c3ffafe86446b7df91a5de
Janat Taerakul <janat.taerakul@gmail.com>
2024-11-26 15:24:27 +0900

    Add script to instal vimplug for nvim

--- scripts/install_other.sh
+++ scripts/install_other.sh
@@ -2,3 +2,5 @@
 
 brew install exiftool
 brew install --cask alt-tab
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
