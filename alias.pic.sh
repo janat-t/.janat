30e2e5573a4bea5a38f2c4b1d055291ae1c68400
Janat Taerakul <janat.taerakul@gmail.com>
2026-05-28 20:44:52 +0900

    Remove test_func dead code; batch exiftool calls in mvpic via pic_dates map

--- alias.pic.sh
+++ alias.pic.sh
@@ -16,10 +16,29 @@ alias uniqdate="picdate *"
 alias cppic='mvpic -c'
 
 picdate() {
-    (for pic in $@; do
-        exiftool -- $pic | grep -E "[12][0-9]{3}:[0-9]{2}:[0-9]{2}" | grep -E "Creat|Modif" | cut -f 2- -d : | awk '{print $1}' | sed "s/:/-/g" | sort | head -n 1
-    done) | sort | uniq
+    if (( ${+pic_dates} )); then
+        for pic in $@; do
+            local d=${pic_dates[$pic]}
+            [[ -n $d ]] && echo $d
+        done | sort | uniq
+    else
+        (for pic in $@; do
+            exiftool -- $pic | grep -E "[12][0-9]{3}:[0-9]{2}:[0-9]{2}" | grep -E "Creat|Modif" | cut -f 2- -d : | awk '{print $1}' | sed "s/:/-/g" | sort | head -n 1
+        done) | sort | uniq
+    fi
+}
 
+_build_pic_dates() {
+    typeset -gA pic_dates
+    pic_dates=()
+    [[ $# -eq 0 ]] && return
+    while IFS=$'\t' read -r fname orig create modify; do
+        for d in $orig $create $modify; do
+            [[ $d =~ '^[12][0-9]{3}:[0-9]{2}:[0-9]{2}' ]] || continue
+            pic_dates[$fname]=${${d:0:10}//:/-}
+            break
+        done
+    done < <(exiftool -T -filename -DateTimeOriginal -CreateDate -ModifyDate -- $@)
 }
 
 get_exts() {
@@ -30,32 +49,6 @@ get_exts() {
     fi
 }
 
-test_func() {
-    a_flag=''
-    b_flag=''
-    files=''
-    verbose='false'
-
-    print_usage() {
-        printf "Usage: ..."
-
-    }
-
-    while getopts 'abf:v' flag; do
-        case "${flag}" in
-            a) a_flag='true' ;;
-            b) b_flag='true' ;;
-            f) files="${OPTARG}" ;;
-            v) verbose='true' ;;
-            *) print_usage
-                exit 1 ;;
-        esac
-    done
-
-    echo $@
-    echo $a_flag $b_flag $verbose
-    echo $files
-}
 
 mvpic() {
     mode='i'
@@ -77,6 +70,8 @@ mvpic() {
     done
 
     exts=($(get_exts $exts))
+    local pics=($(ls_ext $exts))
+    _build_pic_dates $pics
     if [[ $cmd == 'cp' && $mode == 'n' && $(uname -s) == 'Linux' ]]; then
         flags=(--update=none)
         [[ -n $verbose ]] && flags+=(-v)
@@ -89,13 +84,14 @@ mvpic() {
     else
         create_fld $exts
     fi
-    for pic in $(ls_ext $exts); do
+    for pic in $pics; do
         date=$(picdate $pic)
         [[ -z $date ]] && echo $pic doesn\'t have date data && continue
         [[ -z $cfld ]] && fld=${date:0:4} || fld=$cfld
         dst=${$(echo $PIC/$fld/$date*)[-1]}
         $cmd "${flags[@]}" "$pic" "$dst";
     done
+    unset pic_dates
 }
 
 ls_ext() {
